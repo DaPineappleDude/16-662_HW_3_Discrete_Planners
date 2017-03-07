ea7dc62e667f751f56f97acd8f61894c276f09e1
Zihao Zhang <zhzhang0225@gmail.com>
2017-03-06 20:42:14 -0500

    first draft of DFS

--- code/DepthFirstPlanner.py
+++ code/DepthFirstPlanner.py
@@ -4,6 +4,8 @@ class DepthFirstPlanner(object):
         self.planning_env = planning_env
         self.visualize = visualize
         self.nodes = dict()
+        # Key =  node_id (visited)
+        # Val =  parent node_id
 
     def Plan(self, start_config, goal_config):
         
@@ -13,8 +15,47 @@ class DepthFirstPlanner(object):
         #  The return path should be a numpy array
         #  of dimension k x n where k is the number of waypoints
         #  and n is the dimension of the robots configuration space
+        
+        start_id = self.planning_env.discrete_env.ConfigurationToNodeId(start_config)
+        goal_id = self.planning_env.discrete_env.ConfigurationToNodeId(start_config)
+        stack = [start_id]
+        
+        while not stack:
+        # stack not empty
+        	curr_id = stack.pop()
+        		
+        	neighbors = planning_env.GetSuccessors(curr_id)
+        	for elem in range(len(neighbors)):
+        		if elem == goal_id:
+        			self.nodes = self.nodes + {goal_id, elem}
+        			break
+        		# check if the (collision free) neighbor is visited
+                # collision check should be done in GetSuccessors function 
+        	    if elem not in self.nodes:
+        			self.nodes = self.nodes + {elem, curr_id}
+        			stack.append(elem)
+        
+        # initialize plot (only for Simple Environment visualization)
+        if self.visualize and hasattr(self.planning_env, 'InitializePlot'):
+            self.planning_env.InitializePlot(goal_config)
+
+        plan.append(goal_config)
+        node_id = goal_id 
+        last_id = 0
+        last_config = []
+        while self.nodes.get(node_id) != start_id:
+        	last_id = node_id
+        	node_id = self.nodes.get(node_id)
+            node_config = self.planning_env.discrete_env.NodeIdToConfiguration(node_id)
+            last_config = self.planning_env.discrete_env.NodeIdToConfiguration(last_id)
+        	plan.append(node_config)
+            # only for Simple Environment visualization 
+            self.planning_env.PlotEdge(node_config, last_config)
 
         plan.append(start_config)
-        plan.append(goal_config)
-
+        # only for Simple Environment visualization
+        self.planning_env.PlotEdge(start_config, last_config)       
+        
+        plan = plan.reverse()
+        
         return plan
