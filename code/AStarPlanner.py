1d0022f5488398bfc5959eabb9d4e3268f9ae99c
lwan <wanlogan@gmail.com>
2017-03-09 02:45:01 -0500

    almost

--- code/AStarPlanner.py
+++ code/AStarPlanner.py
@@ -20,14 +20,15 @@ class AStarPlanner(object):
         config = self.planning_env.discrete_env.NodeIdToConfiguration(iid)
         return config
 
-    def getLowestId (self, iid, costs):
-        val = 0.0
-        for key in costs:
-            tempval = costs[key]
-            if tempval < val:
-              val = tempval
-              curr_id = key
-        return curr_id      
+    def getLowestId (self, openList, costs): 
+        idx = 1
+        for iid in openList:
+            if idx == 1:
+                lowestId = iid                
+            if costs[iid] < costs[lowestId]:
+              lowestId  = iid
+              
+        return lowestId
 
     def Plan(self, start_config, goal_config):
 
@@ -39,64 +40,62 @@ class AStarPlanner(object):
         #  and n is the dimension of the robots configuration space
 
         if self.visualize and hasattr(self.planning_env, 'InitializePlot'):
-            start_id = self.planning_env.discrete_env.ConfigurationToNodeId(start_config)
-            goal_id = self.planning_env.discrete_env.ConfigurationToNodeId(goal_config)
+            self.planning_env.InitializePlot(goal_config)
 
-            # Open /closed list of nodes / ids
-            openList = collections.deque([start_id])
-            closedList = collections.deque()
-            # Dictionary of edges
-            edges = {}
-            # evaluation cost
-            ecosts = {start_id = 0}
-            # operating cost
-            ocosts = {start_id = 0}
+        start_id = self.planning_env.discrete_env.ConfigurationToNodeId(start_config)
+        goal_id = self.planning_env.discrete_env.ConfigurationToNodeId(goal_config)
 
-            curr_id = start_id
-            while openList:
-                # Save previous id to compoute operating cost
-                prev_id = curr_id
-                # Get lowest evaluation cost ID
-                curr_id = self.getLowestId (curr_id, ecosts)
-                # Remove the best curr_id from open, add to closed
-                openList.remove(curr_id)
-                closedList.append(curr_id)
-                # Get neighbors
-                neighbors = self.planning_env.GetSuccessors(curr_id)
-#                # Initialize the lowest neighbor heuristic cost
-#                bestCost = None
+        # Open /closed list of nodes / ids
+        openList = collections.deque([start_id])
+        closedList = collections.deque()
+        # Dictionary of edges
+        edges = {}
+        # evaluation cost
+        ecosts = {start_id : 0}
+        # operating cost
+        ocosts = {start_id : 0}
 
-                # Compute operating cost
-#                ocosts[curr_id] = self.distance(prev_id, curr_id) + ocosts[prev_id]
+        curr_id = start_id
+        while openList:
+            # Save previous id to compoute operating cost
+#                prev_id = curr_id
+            # Get lowest evaluation cost ID
+            curr_id = self.getLowestId (openList, ecosts)
+            # Remove the best curr_id from open, add to closed
+            import pdb; pdb.set_trace()
+            openList.remove(curr_id)
+            closedList.append(curr_id)
+            # Get neighbors
+            neighbors = self.planning_env.GetSuccessors(curr_id)
 
-                # early exit
-                if curr_id == goal_id:
-                  break
+            # early exit
+            if curr_id == goal_id:
+              break
 
-                for neighbor in neighbors:
-                    # If neighbor is not visited yet; otherwise, move to next neighbor
-                    if neighbor not in closedList:
-                        ocosts[neighbor] = self.distance(curr_id, neighbor)
+            for neighbor in neighbors:
+                # If neighbor is not visited yet; otherwise, move to next neighbor
+                if neighbor not in closedList:
+                    # If neighbor isn't in openlist, add it, precompute evaluation function, operating cost, connect to graph
+                    if neighbor not in openList:                        
+                        
+                        ocosts[neighbor] = self.distance(curr_id, neighbor) + ocosts[curr_id]
+                        ecosts[neighbor] = ocosts[neighbor] + self.heuristic(neighbor, goal_id)
+                        openList.append(neighbor)
+                        edges[neighbor] = curr_id
+                        
+                        # plot edge
+                        if self.visualize:
+                            self.planning_env.PlotEdge(self.planning_env.discrete_env.NodeIdToConfiguration(curr_id),\
+                                                       self.planning_env.discrete_env.NodeIdToConfiguration(neighbor), 'k')
 
-                        # If neighbor isn't in openlist, add it, precompute evaluation function
-                        if neighbor not in openList:
-                            openList.append(neighbor)
-                            ecosts[neighbor] = self.heuristic(neighbor, goal_id) + ocosts[neighbor]
+                    # if neighbor is in open list, then see if it is closer to beginning now than before
+                    else:
+                        currcost = ocosts[neighbor]
+                        updatecost = ocosts[curr_id] + self.distance(curr_id, neighbor)
+                        if updatecost < currcost:
+                            edges[neighbor] = curr_id
 
-                        else:
-                            print curr_id
-                            print neighbor
-                            
-
-
-                            if bestCost == None or fcost < bestCost:
-                                bestCost = fcost
-                                edges[neighbor] = curr_id
-
-                                if self.visualize:
-                                    self.planning_env.PlotEdge(self.planning_env.discrete_env.NodeIdToConfiguration(curr_id),\
-                                                               self.planning_env.discrete_env.NodeIdToConfiguration(neighbor), 'k')
-                          
+                     
         plan_id = goal_id
 
         while plan_id != start_id:
@@ -105,5 +104,6 @@ class AStarPlanner(object):
             import pdb; pdb.set_trace()
             plan_id = edges[plan_id]
             
+        plan.append(start_config)
         plan = plan[::-1]
         return plan
